dcc1a121e5422df339693cd6734004dbd29b557c
Ralph Corderoy <ralph@inputplus.co.uk>
2002-03-05 23:46:46 +0000

    Prototype the functions before calling them.

--- src/swiregister.c
+++ src/swiregister.c
@@ -1,8 +1,17 @@
+#define LIST \
+    X(basictrans) \
+    X(osmisc) \
+    X(sharedclibrary) \
+
+#define X(area) void area ## _swi_register(void);
+LIST
+#undef X
 
 void swi_register_all(void)
 {
-  osmisc_swi_register();
-  basictrans_swi_register();
-  sharedclibrary_swi_register();
-}
+#define X(area) area ## _swi_register();
+    LIST
+#undef X
 
+    return;
+}

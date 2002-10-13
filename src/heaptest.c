92498191de54af51e0ea9116c2c45ce51a588f51
Ralph Corderoy <ralph@inputplus.co.uk>
2002-10-13 10:30:26 +0000

    Make debug output optional with -d.  Accept the number of tests to run on the command line, 0 or none given means infinity.  Print pointers as offsets from the start of the heap to make debug analysis easier.  Check the returned heap pointer is within the heap and so is the last byte of the size claimed.  With these changes, heaptest's assertion always fails on this system using `heaptest -d 1282' with block 53 being allocated so it lies four bytes past the end of the heap.

--- src/heaptest.c
+++ src/heaptest.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <assert.h>
 
 #include <monty/monty.h>
 #include <monty/mem.h>
@@ -16,8 +17,18 @@ typedef struct {
     BYTE content;
 } test;
 
-int main(void)
+#define D if (optd) debug
+#define O(p) ((char *)(p) - (char *)h)
+#define IN_RANGE(x, l, h) ((x) >= (l) && (x) <= (h))
+#define CHECK_PTR(p) \
+    IN_RANGE((char *)(p), (char *)h, (char *)h + HEAP_SIZE - 1)
+
+int main(int argc, char *argv[])
 {
+    int optd;
+    int testnum;
+    int testdec;
+    char **argp;
     test block[100];
     test *b;
     heap_t *h;
@@ -25,7 +36,26 @@ int main(void)
     BYTE *p;
     BYTE *new;
 
+    (progname = strrchr(*argv, '/')) ? progname++ : (progname = *argv);
     debugf = verbosef = stderr;
+
+    optd = 0;
+    testnum = 1;
+    testdec = 0;
+
+    argp = argv + 1;
+    if (*argp && !strcmp(*argp, "-d")) {
+        optd = 1;
+        argp++;
+    }
+    if (*argp) {
+        testnum = strtol(*argp, NULL, 0);
+        testdec = 1;
+        argp++;
+    }
+    if (*argp) {
+        error("usage: %s [-d] [numtest]\n", progname);
+    }
 
     for (b = block; b < END(block); b++) {
         b->data = NULL;
@@ -34,51 +64,58 @@ int main(void)
     }
 
     h = emalloc(HEAP_SIZE);
+    D("heap %#p, size %d\n", h, HEAP_SIZE);
     heap_init(h, HEAP_SIZE);
 
-    while (1) {
+    do {
         b = block + rand() % DIM(block);
         size = ((rand() % (HEAP_SIZE / 8)) + 3) & ~3;
 
-        debug("block %2d, size %3d", b - block, size);
+        D("block %2d, size %3d", b - block, size);
 
         if (b->data) {
             for (p = b->data; p < b->data + b->size; p++) {
                 if (*p != b->content) {
-                    error("content wrong: block %p byte %p was "
-                        "%ud not %ud\n", b->data, p, *p, b->content);
+                    error("content wrong: block +%x byte +%x was "
+                        "%ud not %ud\n", O(b->data), O(p), *p, b->content);
                 }
             }
 
             switch (rand() % 2) {
             case 0:
-                debug(", free\n");
+                D(", free\n");
                 heap_block_free(h, b->data);
                 b->data = NULL;
                 break;
             case 1:
                 if ((new = heap_block_resize(h, b->data, size))) {
-                    debug(", resized %p\n", new);
+                    D(", resized +%d\n", O(new));
+                    assert(CHECK_PTR(new));
+                    assert(CHECK_PTR((char *)new + size - 1));
+
                     b->data = new;
                     b->size = size;
                     memset(b->data, b->content, b->size);
                 } else {
-                    debug(", resize failed\n");
+                    D(", resize failed\n");
                 }
                 break;
             }
         } else {
             if ((new = heap_block_alloc(h, size))) {
-                debug(", allocated %p\n", new);
+                D(", allocated +%d\n", O(new));
+                assert(CHECK_PTR(new));
+                assert(CHECK_PTR((char *)new + size - 1));
+
                 b->data = new;
                 b->size = size;
                 b->content = rand() % 0x100;
                 memset(b->data, b->content, b->size);
             } else {
-                debug(", allocation failed\n");
+                D(", allocation failed\n");
             }
         }
-    }
+    } while (testnum -= testdec);
 
     return 0;
 }

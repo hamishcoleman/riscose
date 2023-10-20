fe46d3a455429049454f3df12cd030e002abf562
Graham Toal <gtoal@gtoal.com>
2023-10-21 00:32:39 +0100

    error() now reports calling function
    Makes tracking unimplemented SWIs a little easier.
    https://github.com/hamishcoleman/riscose/issues/1

--- src/monty/monty.c
+++ src/monty/monty.c
@@ -52,19 +52,48 @@ void warn(char *fmt, ...)
     return;
 }
 
-void error(char *fmt, ...)
+#ifdef error
+#undef error
+#endif
+void error(const char *SWIname, char *fmt, ...)
 {
+    int len;
     va_list args;
+    static FILE *nullf;
 
+    if (nullf == NULL) nullf = fopen("/dev/null", "w");
+
+    if (nullf == NULL) {
+      fprintf(stderr, "%s: error: ", progname);
+      va_start(args, fmt);
+      len = vfprintf(stderr, fmt, args);
+      va_end(args);
+      exit(1);
+    }
+    
+    // determine the size of the string
     va_start(args, fmt);
-
-    fprintf(stderr, "%s: error: ", progname);
-    vfprintf(stderr, fmt, args);
-
+    len = vfprintf(nullf, fmt, args);
     va_end(args);
 
+    char str[len+1]; // and get the string. (gcc extension)
+    
+    va_start(args, fmt);
+    len = vsprintf(str, fmt, args);
+    va_end(args);
+
+    // if it is this particulr string, add in the name of the SWI.
+    if (strcmp(str, "*** SWI unimplemented\n") == 0) {
+      fprintf(stderr, "%s: error: *** SWI %s unimplemented\n", progname, SWIname);
+    } else {
+      fprintf(stderr, "%s: error: %s", progname, str);
+    }
+    
+    fclose(nullf);    
     exit(1);
 }
+
+#define error(fmt...) error(__func__, fmt)
 
 void dump_core(char *fmt, ...)
 {
